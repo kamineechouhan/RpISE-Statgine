06d9869de4f149f09f66c092bdb735c6911b9441
Anuradha Thakor <34335522+anuradhathakor@users.noreply.github.com>
2017-12-25 16:14:32 +0530

    Update Count_Letter_in_Listnames.py
    I have updated this code.
    earlier there were names inside the list.  Now user can input the names inside the list.  
    Then user inputs the single letter and the output will show how many times the given letter appears in the list.

--- Count_Letter_in_Listnames.py
+++ Count_Letter_in_Listnames.py
@@ -1,12 +1,25 @@
-name = ['Anuradha','Yojesh','Suhrad']
-print('Please enter any letter')
-lettersearch=input()
+name = []
+data = ""
 count = 0
+print(range(len(data)))
 
+while data != 'q':
+    print('Please enter your name : ')
+    data = input()
+    name.append(data)
+name.remove(data)
+
+print('Please enter any one letter')
+lettersearch=input()
+
+while len(lettersearch) != 1:
+    print('Please enter correct search')
+    print('Please enter any one letter Again')
+    lettersearch = input()
+
+count = 0
 for v in range(len(name)):
     for i in range(len(name[v])):
        if lettersearch.lower() in name[v][i] or lettersearch.upper() in name[v][i]:
             count += 1
-print(lettersearch,'appeared for',count,'times')
-
-
+print(lettersearch,'appeared for', count, 'times')
